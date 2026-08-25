7e9e181e3e31776dd20941919d1445ffcba48212
kshitij <kalraokshitij@gmail.com>
2026-08-25 22:01:14 +0530

    taking input

--- hello.java
+++ hello.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class hello{
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in); //system.in -> read from the keyboard
+        int age = sc.nextInt();
+        System.out.println("Your age is: " + age);
+
+        String city = sc.next(); //reads only one word
+        System.out.println("Your city is: " + city);
+
+        sc.nextLine(); //consume the leftover newline character because next leaves a newline character in the input buffer
+        //or just use nextLine() everywhere
+
+        String full_name = sc.nextLine();
+        System.out.println("Your full name is: " + full_name);
+        
     }
 }
