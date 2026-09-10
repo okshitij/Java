897fb23546bdfb61c3aa0ab42792de7a8435e06d
kshitij <kalraokshitij@gmail.com>
2026-09-11 00:19:12 +0530

    custom exception

--- exceptions/Custom.java
+++ exceptions/Custom.java
@@ -1,3 +1,23 @@
-public class Custom{
-    
+public class Custom extends Exception{
+    Custom(String message){
+        super(message);
+    }
 }
+
+
+public class Main(){
+    psvm(String[] args){
+        try{
+            method1();
+        }
+        catch(Custom e){
+            //handle exception
+        }
+    }
+
+    psv method1() throws Custom{
+        throw new Custom("some issue arise");
+    }
+}
+        
+ 
